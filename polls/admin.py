b90c16b2bdfa247c14f7d5e290f073104ead3788
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2025-11-19 12:30:44 +0100

    Created Models for the database

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    list_filter = ["created_at"]
+    search_fields = ["title"]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.choice_text}"
