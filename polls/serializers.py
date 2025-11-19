f969ffc1240aa6351b712e1e8783a6114d2d42be
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2025-11-19 13:24:38 +0100

    Set up the serializers

---
+++ polls/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Question, Choice
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = "__all__"
+
+class QuestionSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source = "user.username")
+    choices = ChoiceSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = Question
+        fields = "__all__"
